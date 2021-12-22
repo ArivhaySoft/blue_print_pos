04b0a27ad14b948d712bcef6244c7b6a83ceba64
arif91 <arif.rahman@carsworld.co.id>
2021-12-22 10:13:37 +0700

    IOS sementara dimatikan

--- lib/blue_print_pos.dart
+++ lib/blue_print_pos.dart
@@ -10,15 +10,15 @@ import 'package:blue_print_pos/scanner/blue_scanner.dart';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart' as blue_thermal;
 import 'package:esc_pos_utils_plus/esc_pos_utils.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_blue/flutter_blue.dart' as flutter_blue;
-import 'package:flutter_blue/gen/flutterblue.pb.dart' as proto;
+// import 'package:flutter_blue/flutter_blue.dart' as flutter_blue;
+// import 'package:flutter_blue/gen/flutterblue.pb.dart' as proto;
 import 'package:image/image.dart' as img;
 import 'package:qr_flutter/qr_flutter.dart';
 
 class BluePrintPos {
   BluePrintPos._() {
     _bluetoothAndroid = blue_thermal.BlueThermalPrinter.instance;
-    _bluetoothIOS = flutter_blue.FlutterBlue.instance;
+    // _bluetoothIOS = flutter_blue.FlutterBlue.instance;
   }
 
   static BluePrintPos get instance => BluePrintPos._();
@@ -29,10 +29,10 @@ class BluePrintPos {
   blue_thermal.BlueThermalPrinter? _bluetoothAndroid;
 
   /// This field is library to handle in iOS Platform
-  flutter_blue.FlutterBlue? _bluetoothIOS;
+  // flutter_blue.FlutterBlue? _bluetoothIOS;
 
   /// Bluetooth Device model for iOS
-  flutter_blue.BluetoothDevice? _bluetoothDeviceIOS;
+  // flutter_blue.BluetoothDevice? _bluetoothDeviceIOS;
 
   /// State to get bluetooth is connected
   bool _isConnected = false;
@@ -64,23 +64,23 @@ class BluePrintPos {
                 selectedDevice?.name ?? '', selectedDevice?.address ?? '');
         await _bluetoothAndroid?.connect(bluetoothDeviceAndroid);
       } else if (Platform.isIOS) {
-        _bluetoothDeviceIOS = flutter_blue.BluetoothDevice.fromProto(
-          proto.BluetoothDevice(
-            name: selectedDevice?.name ?? '',
-            remoteId: selectedDevice?.address ?? '',
-            type: proto.BluetoothDevice_Type.valueOf(selectedDevice?.type ?? 0),
-          ),
-        );
-        final List<flutter_blue.BluetoothDevice> connectedDevices =
-            await _bluetoothIOS?.connectedDevices ??
-                <flutter_blue.BluetoothDevice>[];
-        final int deviceConnectedIndex = connectedDevices
-            .indexWhere((flutter_blue.BluetoothDevice bluetoothDevice) {
-          return bluetoothDevice.id == _bluetoothDeviceIOS?.id;
-        });
-        if (deviceConnectedIndex < 0) {
-          await _bluetoothDeviceIOS?.connect();
-        }
+        // _bluetoothDeviceIOS = flutter_blue.BluetoothDevice.fromProto(
+        //   proto.BluetoothDevice(
+        //     name: selectedDevice?.name ?? '',
+        //     remoteId: selectedDevice?.address ?? '',
+        //     type: proto.BluetoothDevice_Type.valueOf(selectedDevice?.type ?? 0),
+        //   ),
+        // );
+        // final List<flutter_blue.BluetoothDevice> connectedDevices =
+        //     await _bluetoothIOS?.connectedDevices ??
+        //         <flutter_blue.BluetoothDevice>[];
+        // final int deviceConnectedIndex = connectedDevices
+        //     .indexWhere((flutter_blue.BluetoothDevice bluetoothDevice) {
+        //   return bluetoothDevice.id == _bluetoothDeviceIOS?.id;
+        // });
+        // if (deviceConnectedIndex < 0) {
+        //   await _bluetoothDeviceIOS?.connect();
+        // }
       }
 
       _isConnected = true;
@@ -104,7 +104,7 @@ class BluePrintPos {
       }
       _isConnected = false;
     } else if (Platform.isIOS) {
-      await _bluetoothDeviceIOS?.disconnect();
+      // await _bluetoothDeviceIOS?.disconnect();
       _isConnected = false;
     }
 
@@ -194,19 +194,19 @@ class BluePrintPos {
       if (Platform.isAndroid) {
         _bluetoothAndroid?.writeBytes(Uint8List.fromList(byteBuffer));
       } else if (Platform.isIOS) {
-        final List<flutter_blue.BluetoothService> bluetoothServices =
-            await _bluetoothDeviceIOS?.discoverServices() ??
-                <flutter_blue.BluetoothService>[];
-        final flutter_blue.BluetoothService bluetoothService =
-            bluetoothServices.firstWhere(
-          (flutter_blue.BluetoothService service) => service.isPrimary,
-        );
-        final flutter_blue.BluetoothCharacteristic characteristic =
-            bluetoothService.characteristics.firstWhere(
-          (flutter_blue.BluetoothCharacteristic bluetoothCharacteristic) =>
-              bluetoothCharacteristic.properties.write,
-        );
-        await characteristic.write(byteBuffer, withoutResponse: true);
+        // final List<flutter_blue.BluetoothService> bluetoothServices =
+        //     await _bluetoothDeviceIOS?.discoverServices() ??
+        //         <flutter_blue.BluetoothService>[];
+        // final flutter_blue.BluetoothService bluetoothService =
+        //     bluetoothServices.firstWhere(
+        //   (flutter_blue.BluetoothService service) => service.isPrimary,
+        // );
+        // final flutter_blue.BluetoothCharacteristic characteristic =
+        //     bluetoothService.characteristics.firstWhere(
+        //   (flutter_blue.BluetoothCharacteristic bluetoothCharacteristic) =>
+        //       bluetoothCharacteristic.properties.write,
+        // );
+        // await characteristic.write(byteBuffer, withoutResponse: true);
       }
     } on Exception catch (error) {
       print('$runtimeType - Error $error');
--- lib/scanner/blue_scanner.dart
+++ lib/scanner/blue_scanner.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:blue_print_pos/models/blue_device.dart';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart' as blue_thermal;
-import 'package:flutter_blue/flutter_blue.dart' as flutter_blue;
+// import 'package:flutter_blue/flutter_blue.dart' as flutter_blue;
 
 /// This class with static method to handler scanning in Android and iOS
 class BlueScanner {
@@ -26,33 +26,33 @@ class BlueScanner {
           )
           .toList();
     } else if (Platform.isIOS) {
-      final flutter_blue.FlutterBlue bluetoothIOS =
-          flutter_blue.FlutterBlue.instance;
-      final List<flutter_blue.BluetoothDevice> resultDevices =
-          <flutter_blue.BluetoothDevice>[];
-
-      await bluetoothIOS.startScan(
-        timeout: const Duration(seconds: 5),
-      );
-      bluetoothIOS.scanResults
-          .listen((List<flutter_blue.ScanResult> scanResults) {
-        for (final flutter_blue.ScanResult scanResult in scanResults) {
-          resultDevices.add(scanResult.device);
-        }
-      });
-
-      await bluetoothIOS.stopScan();
-      devices = resultDevices
-          .toSet()
-          .toList()
-          .map(
-            (flutter_blue.BluetoothDevice bluetoothDevice) => BlueDevice(
-              address: bluetoothDevice.id.id,
-              name: bluetoothDevice.name,
-              type: bluetoothDevice.type.index,
-            ),
-          )
-          .toList();
+      // final flutter_blue.FlutterBlue bluetoothIOS =
+      //     flutter_blue.FlutterBlue.instance;
+      // final List<flutter_blue.BluetoothDevice> resultDevices =
+      //     <flutter_blue.BluetoothDevice>[];
+      //
+      // await bluetoothIOS.startScan(
+      //   timeout: const Duration(seconds: 5),
+      // );
+      // bluetoothIOS.scanResults
+      //     .listen((List<flutter_blue.ScanResult> scanResults) {
+      //   for (final flutter_blue.ScanResult scanResult in scanResults) {
+      //     resultDevices.add(scanResult.device);
+      //   }
+      // });
+      //
+      // await bluetoothIOS.stopScan();
+      // devices = resultDevices
+      //     .toSet()
+      //     .toList()
+      //     .map(
+      //       (flutter_blue.BluetoothDevice bluetoothDevice) => BlueDevice(
+      //         address: bluetoothDevice.id.id,
+      //         name: bluetoothDevice.name,
+      //         type: bluetoothDevice.type.index,
+      //       ),
+      //     )
+      //     .toList();
     }
     return devices;
   }
